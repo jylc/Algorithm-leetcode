045f0bc8b0783c37ddf02266ec9c828f47689308
jylc <wxb1737450829@126.com>
2020-08-18 09:07:45 +0800

    Add 109. 有序链表转换二叉搜索树

---
+++ 101-200/109. 有序链表转换二叉搜索树/main.go
@@ -0,0 +1,61 @@
+package main
+
+//109. 有序链表转换二叉搜索树
+//https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedListToBST(head *ListNode) *TreeNode {
+	return buildTree(head, nil)
+}
+
+func getMedian(left, right *ListNode) *ListNode {
+	fast, slow := left, left
+	for fast != right && fast.Next != right {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}
+
+func buildTree(left, right *ListNode) *TreeNode {
+	if left == right {
+		return nil
+	}
+	mid := getMedian(left, right)
+	root := &TreeNode{
+		Val:   mid.Val,
+		Left:  nil,
+		Right: nil,
+	}
+	root.Left = buildTree(left, mid)
+	root.Right = buildTree(mid.Next, right)
+	return root
+}
+func main() {
+
+}
