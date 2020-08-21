7845e1f06db28b6091af7fadedaeec7653ad2fcc
jylc <wxb1737450829@126.com>
2020-08-21 09:35:23 +0800

    Add 111. 二叉树的最小深度

---
+++ 101-200/111. 二叉树的最小深度/main.go
@@ -0,0 +1,37 @@
+package main
+
+//111. 二叉树的最小深度
+//https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/submissions/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := minDepth(root.Left)
+	right := minDepth(root.Right)
+	if left == 0 || right == 0 {
+		return left + right + 1
+	}
+	if left < right {
+		return left + 1
+	} else {
+		return right + 1
+	}
+}
+func main() {
+
+}
