de1fc9ef972249bea37bd4b910abc225ed2328e8
jylc <wxb1737450829@126.com>
2020-10-01 10:43:16 +0800

    Add LCP 19. 秋叶收藏集

---
+++ LCP/LCP 19. 秋叶收藏集/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "math"
+
+//LCP 19. 秋叶收藏集
+//https://leetcode-cn.com/problems/UlBDOe/
+
+const inf = math.MaxInt32
+
+func minimumOperations(leaves string) int {
+	n := len(leaves)
+	f := make([][3]int, n)
+	f[0][0] = boolToInt(leaves[0] == 'y')
+	f[0][1] = inf
+	f[0][2] = inf
+	f[1][2] = inf
+
+	for i := 1; i < n; i++ {
+		isRed := boolToInt(leaves[i] == 'r')
+		isYellow := boolToInt(leaves[i] == 'y')
+		f[i][0] = f[i-1][0] + isYellow
+		f[i][1] = min(f[i-1][0], f[i-1][1]) + isRed
+		if i >= 2 {
+			f[i][2] = min(f[i-1][1], f[i-1][2]) + isYellow
+		}
+	}
+	return f[n-1][2]
+}
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+func main() {
+
+}
