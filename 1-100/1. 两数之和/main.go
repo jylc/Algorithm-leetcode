a48c6aa8b2753947f9eb083569d33d74004fbd7c
jylc <wxb1737450829@126.com>
2020-10-03 03:07:37 +0800

    Add 1. 两数之和

---
+++ 1-100/1. 两数之和/main.go
@@ -0,0 +1,19 @@
+package main
+
+//1. 两数之和
+//https://leetcode-cn.com/problems/two-sum/
+func twoSum(nums []int, target int) []int {
+	result := []int{}
+	m := make(map[int]int)
+	for i, k := range nums {
+		if value, exist := m[target-k]; exist {
+			result = append(result, value)
+			result = append(result, i)
+		}
+		m[k] = i
+	}
+	return result
+}
+func main() {
+
+}
