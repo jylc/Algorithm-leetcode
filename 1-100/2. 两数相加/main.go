f18cdba6f202c1ed42c9c4ca10b35bbc8eeeade5
jylc <wxb1737450829@126.com>
2020-10-04 10:15:26 +0800

    Add 2. 两数相加

--- 1-100/2. 两数相加/main.go
+++ 1-100/2. 两数相加/main.go
@@ -17,9 +17,11 @@ type ListNode struct {
 
 //go
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	list := &ListNode{0, nil}
-	//这里用一个result，只是为了后面返回节点方便，并无他用
-	result := list
+	list := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	head := list.Next
 	tmp := 0
 	for l1 != nil || l2 != nil || tmp != 0 {
 		if l1 != nil {
@@ -30,11 +32,14 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			tmp += l2.Val
 			l2 = l2.Next
 		}
-		list.Next = &ListNode{tmp % 10, nil}
+		list.Next = &ListNode{
+			Val:  tmp % 10,
+			Next: nil,
+		}
 		tmp = tmp / 10
 		list = list.Next
 	}
-	return result.Next
+	return head
 }
 func main() {
 
