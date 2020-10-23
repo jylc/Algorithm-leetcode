e6c9c22581fe2f392e803888d739663559e4dbab
jylc <wxb1737450829@126.com>
2020-10-23 19:27:39 +0800

    Add 3. 无重复字符的最长子串

--- 1-100/3. 无重复字符的最长子串/main.go
+++ 1-100/3. 无重复字符的最长子串/main.go
@@ -1,7 +1,24 @@
 package main
 
+import "strings"
+
 //3. 无重复字符的最长子串
 //https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+func lengthOfLongestSubstring_1(s string) int { //滑动窗口
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		index := strings.Index(s[start:i], string(s[i]))
+		if index == -1 {
+			if i+1 > end {
+				end = i + 1
+			}
+		} else {
+			start += index + 1
+			end += index + 1
+		}
+	}
+	return end - start
+}
 
 func lengthOfLongestSubstring(s string) int {
 	if s == "" {
