e5fe0c243bbebbc6394960ff0331318dd9b130ee
jylc <wxb1737450829@126.com>
2020-11-20 13:17:21 +0800

    Add 283. 移动零

---
+++ 201-300/283. 移动零/main.go
@@ -0,0 +1,17 @@
+package main
+
+//283. 移动零
+//https://leetcode-cn.com/problems/move-zeroes/
+func moveZeroes(nums []int) {
+	left, right, n := 0, 0, len(nums)
+	for right < n {
+		if nums[right] != 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		}
+		right++
+	}
+}
+func main() {
+
+}
