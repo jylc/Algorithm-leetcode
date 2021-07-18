55833810fdd275f6716d922e31954f86d7d4ddd8
jylc <wxb1737450829@126.com>
2021-07-18 11:16:51 +0800

    面试题 10.02. 变位词组

---
+++ 面试题/10.02-group-anagrams-lcci/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+//面试题 10.02. 变位词组
+//https://leetcode-cn.com/problems/group-anagrams-lcci/
+func groupAnagrams(strs []string) [][]string {
+	strMap := make(map[string][]string)
+	for _, str := range strs {
+		s := sortString(str)
+		strMap[s] = append(strMap[s], str)
+	}
+	ans := make([][]string, 0)
+	for _, v := range strMap {
+		ans = append(ans, v)
+	}
+	fmt.Println(ans)
+	return ans
+}
+
+func sortString(str string) string {
+	tmp := []byte(str)
+	sort.Slice(tmp, func(i, j int) bool {
+		if tmp[i] < tmp[j] {
+			return true
+		}
+		return false
+	})
+	return string(tmp)
+}
+
+func main() {
+
+}
