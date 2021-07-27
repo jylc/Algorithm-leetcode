fd2c562f8ef199fbeb152d7f4d73cb194358f3d3
jylc <wxb1737450829@126.com>
2021-07-27 21:55:32 +0800

    1893. 检查是否区域内所有整数都被覆盖

---
+++ 1800-1900/1893-check-if-all-the-integers-in-a-range-are-covered/main.go
@@ -0,0 +1,22 @@
+package main
+
+//https://leetcode-cn.com/problems/check-if-all-the-integers-in-a-range-are-covered/
+//1893. 检查是否区域内所有整数都被覆盖
+func isCovered(ranges [][]int, left int, right int) bool {
+	for i := left; i <= left; i++ {
+		flag := false
+		for _, r := range ranges {
+			if r[0] <= i && r[1] >= i {
+				flag = true
+				continue
+			}
+		}
+		if flag == false {
+			return false
+		}
+	}
+	return true
+}
+func main() {
+
+}
